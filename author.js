ba105c18322593c4a6e0854578b9dd2889389e62
CecileKafrouni <64487789+CecileKafrouni@users.noreply.github.com>
2021-01-02 20:42:46 +0100

    Create author.js

---
+++ author.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/authortest', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+const authorSchema = mongoose.model('Author',{
+    firstName: String,
+    lastName: String
+});
+
+const testAuthor = () => {
+    const author = new authorSchema({firstName: "test", lastName: "html"});
+    author.save().then(() => console.log('ok'));
+}
+
+module.exports = {testAuthor,authorSchema};

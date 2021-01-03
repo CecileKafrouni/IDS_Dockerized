82c8bd256a699794832154f5092e9ac530fd2b19
CecileKafrouni <64487789+CecileKafrouni@users.noreply.github.com>
2021-01-03 21:01:16 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,69 +1,18 @@
-/*
-const app = new (require('koa'))();
-const http = require('http');
-const formidable = require('formidable');
-const fs = require('fs');
+const {testAuthor} = require('./db/schemaFileDb');
 
-
-const {router} = require('./router');
-const serve = require('koa-static');
-
-app.use(require('koa-logger')());
-app.use(require('koa-body')());
-
-//app.use(serve('app'));
-app.use(router.routes());
-
-app.listen(3000);
-*/
-
-const {testAuthor} = require('./author');
-const {test} = require('./upload');
-
-const http = require('http');
-const formidable = require('formidable');
-const fs = require('fs');
+const {fileToUploadForm, storeAndTrainForm, isStored} = require('./view/interface');
 
 const express = require('express');
 const app = express();
 
-app.get('/',(req, res) => {
-    res.send('Hello world')
-});
+app.get('/', fileToUploadForm);
 
-app.get('/test', (req,res) => {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
-    res.write('<input type="file" name="filetoupload"><br>');
-    res.write('<input type="submit">');
-    res.write('</form>');
-    return res.end();
-});
+app.post('/train',storeAndTrainForm);
 
-app.post('/fileupload',(req, res) => {
-    const form = new formidable.IncomingForm();
-    form.parse(req, function (err, fields, files) {
-        const oldpath = files.filetoupload.path;
-        //const newpath = 'C:/Users/cecil/Desktop/JS/Projet/storage/' + files.filetoupload.name;
-        const newpath = 'C:/Users/cecil/Desktop/JS/Projet/storage/' + 'testauthor2.csv';
-        if (fs.existsSync(newpath)) {
-            fs.unlinkSync(newpath);
-        }
-        fs.rename(oldpath, newpath, function (err) {
-            if (err) throw err;
-            res.write('<form action="upload" method="post" enctype="multipart/form-data">');
-            res.write('File uploaded and moved!');
-            res.write('<input type="button" value="Train"><br>');
-            res.write('<input type="submit">');
-            res.write('</form>');
-            res.end();
-        });
-    });
-});
+app.post('/store', isStored);
 
+//app.get('/ajoutauthor', testAuthor);
 
-app.get('/ajoutauthor', testAuthor);
-app.get('/upload', test);
 
 app.listen(3000);
 
